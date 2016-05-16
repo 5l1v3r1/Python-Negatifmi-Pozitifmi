f3053149d81eceddf30bd00128000feba2e1bf72
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-05-16 14:35:15 -0400

    Add files via upload

---
+++ negatifmi_pozitifmi.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+negatifmi_pozitifmi_ico = """
+#################################################################
+#     PYTHON - Negatif mi ? Pozitif mi ? - GH0ST S0FTWARE       #
+################################################################# 
+#                           CONTACT                             #
+#################################################################
+#                  DEVELOPER : İSMAİL TAŞDELEN                  #                       
+#               GMAIL : pentestdatabase@gmail.com               #
+#       Linkedin : https://www.linkedin.com/in/ismailtasdelen   #
+#                 Whatsapp : + 90 534 295 94 31                 #
+#################################################################
+"""
+
+print negatifmi_pozitifmi_ico
+
+sayi = int(input("Bir sayı giriniz : "))
+
+negatif_mesaj_ico = "Girdiğiniz sayı negatifdir."
+pozitif_mesaj_ico = "Girdiğiniz sayı pozitifdir."
+notr_mesaj_ico = "Girdiğiniz sayı nötrdür."
+
+if sayi < 0:
+	print "Sonuç : " + negatif_mesaj_ico
+
+elif sayi == 0:
+	print "Sonuç : " + notr_mesaj_ico
+
+else:
+	print "Sonuç : " + pozitif_mesaj_ico
